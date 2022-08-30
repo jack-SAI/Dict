4388901265b46827b7df20f20b88d02872fda545
jack-SAI <2449001989@qq.com>
2022-08-30 20:16:52 +0800

    master第一次填充。搜索v0.4基础上，增加小功能的登录跳转

---
+++ testing/dbtable.cpp
@@ -0,0 +1,24 @@
+﻿#include "dbtable.h"
+#include "ui_dbtable.h"
+
+dbtable::dbtable(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::dbtable)
+{
+
+    qDebug()<<"@@@@@@@";
+
+    QSqlDatabase db1 = QSqlDatabase::addDatabase("QSQLITE");
+    db1.setDatabaseName("testing.db");
+    if(!db1.open()){
+        qDebug()<<"open db error";
+    }
+
+
+
+}
+
+dbtable::~dbtable()
+{
+    delete ui;
+}
---
+++ testing/mainwindow.cpp
@@ -0,0 +1,47 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <workingplane.h>
+#include <qdebug.h>
+#include <qmessagebox.h>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowFlags(windowFlags()|Qt::WindowMaximizeButtonHint);
+    setWindowFlags(windowFlags()|Qt::WindowMinMaxButtonsHint);
+    setWindowFlags(windowFlags()|Qt::WindowCloseButtonHint);
+    workingPlane *wk = new workingPlane();
+    //db=new dbtable();
+    //wk->setParent(this);
+    //wk->show();
+    connect(ui->templogin,&QPushButton::clicked,wk,&QWidget::show);
+    connect(this,&MainWindow::acception,wk,&QWidget::show);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    qDebug()<<"click templogin";
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    if(ui->lineEdit->text()=="1" && ui->lineEdit_2->text()=="1"){
+        acception();
+    }
+    else{
+
+        QMessageBox::warning(this,"Attention","account or password error!",QMessageBox::Yes);
+        ui->lineEdit->clear();
+        ui->lineEdit_2->clear();
+        ui->lineEdit->setFocus();
+
+    }
+}
